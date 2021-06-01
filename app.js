74b39a2746c38c9f4be04f54592efa42bfd0a615
The Easy Way <74107947+The-Easy-Way@users.noreply.github.com>
2021-06-01 04:06:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const first = document.querySelector('.first');
+const iframe = document.querySelector('iframe');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click',function(){
+    var html = first.textContent
+    iframe.src = "data:text/html;charset=utf-8," + encodeURI(html)
+})
+
+first.addEventListener('keyup',function(){
+    var html = first.textContent
+    iframe.src = "data:text/html;charset=utf-8," + encodeURI(html)
+})
+
+first.addEventListener('paste',function(e){
+e.preventDefault()
+
+var text = e.clipboardData.getData("text/plain")
+
+document.execCommand("insertText", false,text)
+})
